9dc85af7244c20e917f4a9955e1bbf20ac78da49
mohammad M salahat <msalahat88@gmail.com>
2018-05-07 01:46:22 +0300

    change

--- controllers/Class_room_daysController.php
+++ controllers/Class_room_daysController.php
@@ -6,15 +6,65 @@
  * Time: 04:27 ص
  */
 namespace app\controllers;
-use app\models\Users;
-use opensooq\firebase\FirebaseNotifications;
-use yii\helpers\Json;
 use yii\rest\ActiveController;
+use yii;
 class Class_room_daysController extends ActiveController{
     public $modelClass = 'app\models\ClassRoomDays';
     public $serializer = [
         'class' => 'yii\rest\Serializer',
         'collectionEnvelope' => 'items',
     ];
+    public function actions() {
+        $actions = parent::actions();
+        unset($actions[ 'index']);
+        return $actions;
+    }
+    public $prepareDataProvider;
+    public $dataFilter;
 
+    public function actionIndex() {
+        $requestParams = Yii::$app->getRequest()->getBodyParams();
+        if (empty($requestParams)) {
+            $requestParams = Yii::$app->getRequest()->getQueryParams();
+        }
+
+        $filter = null;
+        if ($this->dataFilter !== null) {
+            $this->dataFilter = Yii::createObject($this->dataFilter);
+            if ($this->dataFilter->load($requestParams)) {
+                $filter = $this->dataFilter->build();
+                if ($filter === false) {
+                    return $this->dataFilter;
+                }
+            }
+        }
+
+        if ($this->prepareDataProvider !== null) {
+            return call_user_func($this->prepareDataProvider, $this, $filter);
+        }
+
+        /* @var $modelClass \yii\db\BaseActiveRecord */
+        $modelClass = $this->modelClass;
+
+        $query = $modelClass::find();
+
+        if (!empty($filter)) {
+            $query->andWhere($filter);
+        }
+        $query->with(['classRoom','course']);
+
+
+
+
+        return Yii::createObject([
+            'class' => yii\data\ActiveDataProvider::className(),
+            'query' => $query,
+            'pagination' => [
+                'params' => $requestParams,
+            ],
+            'sort' => [
+                'params' => $requestParams,
+            ],
+        ]);
+    }
 }
--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -1,21 +1,73 @@
 <?php
 namespace app\controllers;
 use app\models\PasswordForm;
-use app\models\User;
 use app\models\Users;
-
-use opensooq\firebase\FirebaseNotifications;
 use sngrl\PhpFirebaseCloudMessaging\Client;
 use sngrl\PhpFirebaseCloudMessaging\Message;
 use sngrl\PhpFirebaseCloudMessaging\Notification;
 use sngrl\PhpFirebaseCloudMessaging\Recipient\Device;
 use yii\rest\ActiveController;
+use yii;
 class UsersController extends ActiveController{
     public $modelClass = 'app\models\Users';
     public $serializer = [
         'class' => 'yii\rest\Serializer',
         'collectionEnvelope' => 'items',
     ];
+
+    public function actions() {
+        $actions = parent::actions();
+        unset($actions[ 'index']);
+        return $actions;
+    }
+    public $prepareDataProvider;
+    public $dataFilter;
+
+    public function actionIndex() {
+        $requestParams = Yii::$app->getRequest()->getBodyParams();
+        if (empty($requestParams)) {
+            $requestParams = Yii::$app->getRequest()->getQueryParams();
+        }
+
+        $filter = null;
+        if ($this->dataFilter !== null) {
+            $this->dataFilter = Yii::createObject($this->dataFilter);
+            if ($this->dataFilter->load($requestParams)) {
+                $filter = $this->dataFilter->build();
+                if ($filter === false) {
+                    return $this->dataFilter;
+                }
+            }
+        }
+
+        if ($this->prepareDataProvider !== null) {
+            return call_user_func($this->prepareDataProvider, $this, $filter);
+        }
+
+        /* @var $modelClass \yii\db\BaseActiveRecord */
+        $modelClass = $this->modelClass;
+
+        $query = $modelClass::find();
+
+        if (!empty($filter)) {
+            $query->andWhere($filter);
+        }
+        $query->with(['classRooms','courses','schedules','classRooms0','tasks','type','branch']);
+
+
+
+
+        return Yii::createObject([
+            'class' => yii\data\ActiveDataProvider::className(),
+            'query' => $query,
+            'pagination' => [
+                'params' => $requestParams,
+            ],
+            'sort' => [
+                'params' => $requestParams,
+            ],
+        ]);
+    }
 
     public function actionType($id){
         return Users::find()->where(['type_id'=>$id])->all();
